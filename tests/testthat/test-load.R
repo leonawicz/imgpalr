711ea01c754a5fc7cef50d3968bec8a1f90fc0f1
Matthew Leonawicz <matt_leonawicz@esource.com>
2019-07-08 17:49:00 -0600

    Update tests.

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -3,12 +3,22 @@ context("file load")
 test_that("image_load loads file", {
   x <- paste0(system.file(package = "imgpalr"), "/blue-yellow.", # nolint
     c("png", "jpg", "bmp", "gif"))
+  url <- "https://raw.githubusercontent.com/leonawicz/imgpalr/master/inst/"
+
   if(require(png)){
     expect_is(image_load(x[1]), "array")
-    url <- "https://raw.githubusercontent.com/leonawicz/imgpalr/master/man/figures/logo.png"
-    expect_is(image_load(url), "array")
+    expect_is(image_load(paste0(url, basename(x[1]))), "array")
   }
-  if(require(jpeg)) expect_is(image_load(x[1]), "array")
-  if(require(bmp)) expect_is(image_load(x[1]), "array")
-  if(require(magick)) expect_is(image_load(x[1]), "array")
+  if(require(jpeg)){
+    expect_is(image_load(x[2]), "array")
+    expect_is(image_load(paste0(url, basename(x[2]))), "array")
+  }
+  if(require(bmp)){
+    expect_is(image_load(x[3]), "array")
+    expect_is(image_load(paste0(url, basename(x[3]))), "array")
+  }
+  if(require(magick)){
+    expect_is(image_load(x[4]), "array")
+    expect_is(image_load(paste0(url, basename(x[4]))), "array")
+  }
 })
